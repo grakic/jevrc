cbea30383fbaa820e3d8c78d3955d9aacb84e287
Goran Rakic <grakic@devbase.net>
2014-10-20 03:12:06 +0200

    Prettify demo applet

--- evrcapplet/src/main/java/net/devbase/jevrc/applet/EvrcApplet.java
+++ evrcapplet/src/main/java/net/devbase/jevrc/applet/EvrcApplet.java
@@ -20,40 +20,64 @@ public class EvrcApplet extends Applet implements ReaderListener {
 
     JSObject window = null;
     EvrcCard card = null;
+    String terminalName = null;
+    String lastError = null;
     String removedCallback = "removed";
     String insertedCallback = "inserted";
+    String errorCallback = "onerror";
 
     @Override
     public void init() {
+
+        // get callbacks
+        removedCallback = getParameter("RemovedCallback");
+        insertedCallback = getParameter("InsertedCallback");
+        errorCallback = getParameter("ErrorCallback");
+
         // pick the first terminal
         CardTerminal terminal = null;
         try {
-            TerminalFactory factory = TerminalFactory.getDefault();
+            TerminalFactory factory = TerminalFactory.getInstance("PC/SC", null);
             List<CardTerminal> terminals = factory.terminals().list();
             terminal = terminals.get(0);
+            terminalName = terminal.getName();
         } catch(Exception e) {
-            stop();
+            notifyError(e.getMessage());
+            return;
         }
 
         // build a reader
         Reader reader = new Reader(terminal);
         reader.addCardListener(this);
-
-        // get callbacks
-        removedCallback = getParameter("RemovedCallback");
-        insertedCallback = getParameter("InsertedCallback");
     }
 
     /* export public function as the Mozilla Bug 606737 workaround */
     public void setupJSObject() {
-        if((window = JSObject.getWindow(this)) == null) {
-            stop();
+        if((window = JSObject.getWindow(this)) != null) {
+            notifyError(null);
         }
     }
 
     @Override
     public void start() {
-        setupJSObject();
+        if(window == null) {
+            setupJSObject();
+        }
+    }
+
+    private void notifyError(String error) {
+        showStatus("Error.");
+        if (error != null) {
+            lastError = error;
+        }
+        if (window != null && lastError != null) {
+            window.call(errorCallback, new Object[] {lastError});
+            lastError = null;
+        }
+    }
+
+    public String getTerminalName() {
+        return terminalName;
     }
 
     public void inserted(EvrcCard card) {
